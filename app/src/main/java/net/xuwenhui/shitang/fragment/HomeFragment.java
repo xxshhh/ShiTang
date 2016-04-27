f0995a57d46bfc6755af01ee1f303a5c28be87b1
xuwenhui <398279436@qq.com>
2016-04-27 21:04:41 +0800

    添加FAB按钮，新增功能：商户可筛选。

--- app/src/main/java/net/xuwenhui/shitang/fragment/HomeFragment.java
+++ app/src/main/java/net/xuwenhui/shitang/fragment/HomeFragment.java
@@ -1,15 +1,23 @@
 package net.xuwenhui.shitang.fragment;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
 import net.xuwenhui.model.Shop;
 import net.xuwenhui.shitang.R;
@@ -33,8 +41,20 @@ public class HomeFragment extends BaseFragment implements BaseSliderView.OnSlide
 
 	@Bind(R.id.slider_show)
 	SliderLayout mSliderShow;
+	@Bind(R.id.tv_display_name)
+	TextView mTvDisplayName;
+	@Bind(R.id.layout)
+	LinearLayout mLayout;
 	@Bind(R.id.list_shop)
 	RecyclerView mListShop;
+	@Bind(R.id.fab_a)
+	FloatingActionButton mFabA;
+	@Bind(R.id.fab_b)
+	FloatingActionButton mFabB;
+	@Bind(R.id.fab_c)
+	FloatingActionButton mFabC;
+	@Bind(R.id.fab_menu)
+	FloatingActionsMenu mFabMenu;
 
 	@Override
 	protected int getContentLayoutId() {
@@ -47,6 +67,8 @@ public class HomeFragment extends BaseFragment implements BaseSliderView.OnSlide
 		initImageDisplay();
 		// 初始化商店列表
 		initShopList();
+		// 初始化FAB
+		initFAB();
 	}
 
 	@Override
@@ -107,6 +129,96 @@ public class HomeFragment extends BaseFragment implements BaseSliderView.OnSlide
 		mListShop.setAdapter(adapter);
 	}
 
+	/**
+	 * 初始化FAB
+	 */
+	private void initFAB() {
+		// FabA：选择食堂
+		Drawable drawableA = new IconicsDrawable(mContext)
+				.icon(GoogleMaterial.Icon.gmd_location_city)
+				.color(mContext.getResources().getColor(R.color.colorAccent))
+				.sizeDp(30);
+		mFabA.setIconDrawable(drawableA);
+		mFabA.setTitle("选择食堂");
+		mFabA.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				T.show(mContext, "选择食堂");
+				new MaterialDialog.Builder(mContext)
+						.items("全部", "1食堂", "2食堂", "3食堂", "6食堂", "7食堂")
+						.itemsCallbackSingleChoice(0, new MaterialDialog.ListCallbackSingleChoice() {
+							@Override
+							public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+								T.show(mContext, text);
+								switch (which) {
+									case 0:
+										mTvDisplayName.setText("全部商家");
+										mFabMenu.collapse();
+										break;
+									case 1:
+										mTvDisplayName.setText("1食堂商家");
+										mFabMenu.collapse();
+										break;
+									case 2:
+										mTvDisplayName.setText("2食堂商家");
+										mFabMenu.collapse();
+										break;
+									case 3:
+										mTvDisplayName.setText("3食堂商家");
+										mFabMenu.collapse();
+										break;
+									case 4:
+										mTvDisplayName.setText("6食堂商家");
+										mFabMenu.collapse();
+										break;
+									case 5:
+										mTvDisplayName.setText("7食堂商家");
+										mFabMenu.collapse();
+										break;
+									default:
+										return true;
+								}
+								return true;
+							}
+						})
+						.positiveText(R.string.choose)
+						.show();
+			}
+		});
+
+		// FabB：销量最高
+		Drawable drawableB = new IconicsDrawable(mContext)
+				.icon(GoogleMaterial.Icon.gmd_whatshot)
+				.color(mContext.getResources().getColor(R.color.colorAccent))
+				.sizeDp(30);
+		mFabB.setIconDrawable(drawableB);
+		mFabB.setTitle("销量最高");
+		mFabB.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				T.show(mContext, "销量最高");
+				mTvDisplayName.setText("销量最高商家");
+				mFabMenu.collapse();
+			}
+		});
+
+		// FabC：评分最高
+		Drawable drawableC = new IconicsDrawable(mContext)
+				.icon(GoogleMaterial.Icon.gmd_star_border)
+				.color(mContext.getResources().getColor(R.color.colorAccent))
+				.sizeDp(30);
+		mFabC.setIconDrawable(drawableC);
+		mFabC.setTitle("评分最高");
+		mFabC.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				T.show(mContext, "评分最高");
+				mTvDisplayName.setText("评分最高商家");
+				mFabMenu.collapse();
+			}
+		});
+	}
+
 	@Override
 	public void onResume() {
 		mSliderShow.startAutoCycle(6000, 6000, true);
@@ -123,4 +235,5 @@ public class HomeFragment extends BaseFragment implements BaseSliderView.OnSlide
 	public void onSliderClick(BaseSliderView slider) {
 		T.show(mContext, slider.getDescription());
 	}
+
 }
